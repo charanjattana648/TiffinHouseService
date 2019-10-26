8688a7ba24abc1cb903d3777d9564e643327790f
charanjattana648 <singhc5@douglascollege.ca>
2019-10-26 10:28:44 -0700

    Dealer class added

--- inc/Entities/Dealer.class.php
+++ inc/Entities/Dealer.class.php
@@ -3,13 +3,16 @@ class Dealer extends User{
     private $companyName;
     private $isVerified=false;
 
-    public function __construct($firstName,$lastName,$userEmail,$password,$phoneNumber,$address,$postalCode,$companyName){
+    public function __construct($firstName,$lastName,$userEmail,$password,$phoneNumber,$address,$postalCode,$companyName,$isVerified=false){
         parent::__construct($firstName,$lastName,$userEmail,$password,$phoneNumber,$address,$postalCode);
+        $this->companyName=$companyName;
+        $this->isVerified=$isVerified;
     }
     public function setCompanyName(string $companyName)
     {
         $this->companyName=$companyName;
     }
+
     public function getcompanyName():string
     {
         return $this->companyName;
