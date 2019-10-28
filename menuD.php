9dc6f0d407571ac1b5b1d71f406d801b5919606f
charanjattana648 <singhc5@douglascollege.ca>
2019-10-28 15:20:19 -0700

    MenuD.php added

--- menuD.php
+++ menuD.php
@@ -7,23 +7,23 @@ include("./views/signUp.php");
 include("./views/login.php");
 require_once ("./inc/Utility/auth.php");
 require_once  ("inc/Entities/User.class.php");
-require_once  ("./inc/Entities/MenuD.class.php");
+require_once  ("./inc/Entities/Menu.class.php");
 require_once ("./inc/Utility/db.php");
 require_once ("./inc/Utility/PDOAgent.class.php");
 //require_once ("./inc/config.inc.php");
 ?>
 <div id="dealerMenu">
 <div >
-<form id="dForm" method="Post" action="menuD.php">
+<form id="dForm" method="Post" action="menuD.php" enctype="multipart/form-data">
 <select name="day" id="day">
     <option value="">Select Day</option>
-    <option>Monday</option>
-    <option>Tuesday</option>
-    <option>Wednesday</option>
-    <option>Thrusday</option>
-    <option>Friday</option>
-    <option>Saturday</option>
-    <option>Sunday</option>
+    <option value="1">Monday</option>
+    <option value="2">Tuesday</option>
+    <option value="3">Wednesday</option>
+    <option value="4">Thrusday</option>
+    <option value="5">Friday</option>
+    <option value="6">Saturday</option>
+    <option value="7">Sunday</option>
 </select><br><br>
 <label for="itemName">ItemName</label>
 	<input type="text" name="itemName" id="itemName" placeholder="enter itemName"/><br>
@@ -59,26 +59,53 @@ if(isset($_POST['getItem']))
     echo "Items fetched are : <br>";
     $db=new database();
 
-    $db::initialize("Menu1");
+    $db::initialize("Menu");
     $menu=$db::getMeal();
     // var_dump($menu);
     $x=count($menu);
-    echo '<div>
- 
-    Monday';
+   
     $days=array();
+    $isNewDay=true;
     
     foreach($menu as $m)
     {
         // echo $m->getItemDetail()."<br>";
         // echo $m->getIngredient()."<br>";
-
-        echo'<div class="hover_menu_img"><a href="##"><img src="images/menu_chole-recipe.png"/> </a>
+     
+        if(in_array($m->getDay(),$days))
+        {
+            $isNewDay=false;
+        }else{
+            array_push($days,$m->getDay());
+            $isNewDay=true;
+        }
+        if($isNewDay)
+        {
+          echo '<div class="menutable">'.$m->getDay();
+        }
+        if($m->getItemImage()!=null)
+        {
+            echo '<a href="##"><img src="data:image/png;base64,'.base64_encode($m->getItemImage()).'"/> </a>';
+        }
+        echo'<div class="hover_menu_img">
         <p>'.$m->getItemName().' </p>
-        <p>$'. $m->getItemPrice().'</p>
+        <p>$'. $m->getItemPrice().'</p>        
         </div>';
     }
     echo' </div>';
+}
+if(isset($_POST['submitItem']))
+{
+    $menu=new Menu();
+    $binary = addslashes(file_get_contents($_FILES['itemImage']['tmp_name']));
+    $menu->saveMenu($_POST['itemName'],$_POST['itemPrice'],$binary,
+    $_POST['itemDetail'],$_POST['ingredient'],"Monday",$_POST['day']);
+    
+    $db=new database();
+
+    $db::initialize("Menu");
+    $mealName=$db::addMeal($menu);
+    echo $mealName." is Name";
 }
 ?>
 
