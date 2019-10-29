675807eb5365fcf86e5ffdda94d1bf074db60967
charanjattana648 <singhc5@douglascollege.ca>
2019-10-29 00:11:39 -0700

    Modified db.php by adding deleteMeal and updateMeal function

--- inc/Utility/db.php
+++ inc/Utility/db.php
@@ -22,7 +22,6 @@ class database{
             $menu=new Menu();
             self::$db->query($menu_query);
             self::$db->execute();
-          //  var_dump(self::$db->resultSet());
             $menu=self::$db->resultSet();
 
             return $menu;
@@ -49,8 +48,40 @@ class database{
              self::$db->bind(':companyName',"Happy");
              self::$db->bind(':dayId',$menu->getDayId());
              self::$db->execute();
-             
-		     $mealId=self::$db->lastInsertedId();
+         }catch(PDOException $err)
+         {
+            echo "Error : ".$err->getMessage();
+         }
+    }
+
+    public static function deleteMeal($id)
+    {
+        $menu_delete_query="DELETE FROM  TiffinHouseDb.Menu WHERE menuId=:id";
+        try{
+            self::$db->query($menu_delete_query);
+            self::$db->bind(':id',$id);
+            self::$db->execute();
+        }catch(PDOException $err)
+        {
+           echo "Error : ".$err->getMessage();
+        }
+    }
+    public static function updateMeal(Menu $menu)
+    {         
+        $menu_add_query="UPDATE TiffinHouseDb.Menu SET itemName=:itemName,itemPrice=:itemPrice,itemImage=:itemImage,itemDetail=:itemDetail,
+        ingredient=:ingredient,day=:day,companyName=:companyName,dayId=:dayId where menuId=:menuId";
+         try{
+             self::$db->query($menu_add_query);
+             self::$db->bind(':menuId',$menu->getItemId());
+             self::$db->bind(':itemName',$menu->getItemName());
+             self::$db->bind(':itemPrice',$menu->getItemPrice());
+             self::$db->bind(':itemImage',$menu->getItemImage());
+             self::$db->bind(':itemDetail',$menu->getItemDetail());
+             self::$db->bind(':ingredient',$menu->getIngredient());
+             self::$db->bind(':day',$menu->getDay());
+             self::$db->bind(':companyName',"Happy");
+             self::$db->bind(':dayId',$menu->getDayId());
+             self::$db->execute();
          }catch(PDOException $err)
          {
             echo "Error : ".$err->getMessage();
