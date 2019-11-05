263b822fe3dde767949d5a88291c21ab0d64eea2
charanjattana648 <singhc5@douglascollege.ca>
2019-11-04 17:54:47 -0800

    menu.php is modified

--- menu.php
+++ menu.php
@@ -1,160 +1,103 @@
-<?php
+
+<?php         
+require_once("inc/config.inc.php");
 include ("./views/header.php");
 include("./views/signUp.php");
 include("./views/login.php");
+require_once ("./inc/Utility/auth.php");
+require_once  ("inc/Entities/User.class.php");
+require_once  ("./inc/Entities/Menu.class.php");
+require_once ("./inc/Utility/db.php");
+require_once ("./inc/Utility/PDOAgent.class.php");
+//require_once ("./inc/config.inc.php");
 ?>
-	
-	<div class="menu">
-	<h1>Weekly Menu</h1>
-	<table class="menutable">
-  <tr>
-    <th>Day</th>
-    <th>Menu</th>
-  </tr>
-  <tr>
-    <td>Monday</td>
-    <td>
-    <div class="hover_menu_img"><a href="#veg_cauili"><img src="images/menu_chole-recipe.png"/> </a>
-    <p>Chole Bature </p>
-    <p>$15</p>
-    </div>
-       <div class="hover_menu_img"> <a href="##"> <img src="images/menu_mix_veg.png"/></a>
-         <p>Chole Bature </p>
-    <p>$15</p></div>
-        <div class="hover_menu_img"> <a href="##"><img src="images/menu_chapati.png"/></a>
-          <p>Chole Bature </p>
-    <p>$15</p></div> 
-  
-  </td>
-    <!--<td>Chick Peas + Aloo Shimla Mirch+2 Naan</td>-->
-  </tr>
-  <tr>
-    <td>Tuesday</td>
-    <td>
-    <div class="hover_menu_img"><a href="##"><img src="images/Methi_malai_matar_gravy@palates_desire.jpg"/> </a>
-    <p>Methi Malai Matar </p>
-    <p>$15</p>
-    </div>
-       <div class="hover_menu_img"> <a href="##"> <img src="images/menu_mix_veg.png"/></a>
-         <p>Chole Bature </p>
-    <p>$15</p></div>
-        <div class="hover_menu_img"> <a href="##"><img src="images/menu_chapati.png"/></a>
-          <p>Chole Bature </p>
-    <p>$15</p></div></td>
-  </tr>
-  <tr>
-    <td>Wednessday</td>
-   <td>
-    <div class="hover_menu_img"><a href="##"><img src="images/IMG_20150316_064733_2.jpg"/> </a>
-    <p>Green Mong Daal</p>
-    <p>$15</p>
-    </div>
-       <div class="hover_menu_img"> <a href="##"> <img src="images/indian-cauliflower-potato-aloo-gobi.jpg"/></a>
-         <p>Aloo Gobhi</p>
-    <p>$15</p></div>
-        <div class="hover_menu_img"> <a href="##"><img src="images/menu_chapati.png"/></a>
-          <p>Chole Bature </p>
-    <p>$15</p></div></td>
-  </tr>
-  <tr>
-    <td>Thursday</td>
-    <td>
-    <div class="hover_menu_img"><a href="##"><img src="images/menu_chole-recipe.png"/> </a>
-    <p>Chole Bature </p>
-    <p>$15</p>
-    </div>
-       <div class="hover_menu_img"> <a href="##"> <img src="images/menu_mix_veg.png"/></a>
-         <p>Chole Bature </p>
-    <p>$15</p></div>
-        <div class="hover_menu_img"> <a href="##"><img src="images/menu_chapati.png"/></a>
-          <p>Chole Bature </p>
-    <p>$15</p></div></td>
-  </tr>
-  <tr>
-    <td>Friday</td>
-   <td>
-    <div class="hover_menu_img"><a href="##"><img src="images/menu_chole-recipe.png"/> </a>
-    <p>Chole Bature </p>
-    <p>$15</p>
-    </div>
-       <div class="hover_menu_img"> <a href="##"> <img src="images/menu_mix_veg.png"/></a>
-         <p>Chole Bature </p>
-    <p>$15</p></div>
-        <div class="hover_menu_img"> <a href="##"><img src="images/menu_chapati.png"/></a>
-          <p>Chole Bature </p>
-    <p>$15</p></div></td>
-  </tr>
-  <tr>
-    <td>Saturday</td>
-    <td>
-    <div class="hover_menu_img"><a href="##"><img src="images/menu_chole-recipe.png"/> </a>
-    <p>Chole Bature </p>
-    <p>$15</p>
-    </div>
-       <div class="hover_menu_img"> <a href="##"> <img src="images/menu_mix_veg.png"/></a>
-         <p>Chole Bature </p>
-    <p>$15</p></div>
-        <div class="hover_menu_img"> <a href="##"><img src="images/menu_chapati.png"/></a>
-          <p>Chole Bature </p>
-    <p>$15</p></div></td>
-  </tr>
-</table>
+<h2><?php if(isset($_SESSION['companyName'])){echo $_SESSION['companyName']." ";}?>Menu</h2>
+<?php
 
-<section class="Recipe">
-<section id="veg_cauili" class="vegMenu">
-<section class="leftphoto">
-<img class="menuimage" src="images/indian-cauliflower-potato-aloo-gobi.jpg" alt="male">
-</section>
-<section class="righttext">
-<h1>Aloo Gobhi</h1><p>Aloo Gobhi is our go-to weeknight cauliflower curry with  potatoes, onions, tomatoes and some of our favorite spices. Usually paired with spinach daal and rice or simply served with warm homemade parathas, it makes a nourishing weeknight meal.</p>
-</section>
-</section>
-<section class="vegMenu">
-<section class="leftphoto">
-<a href="https://thebusybaker.ca/easy-one-pan-lentil-daal-curry/"><img class="menuimage" src="images/Easy-One-Pot-Lentil-Curry-2.jpg" alt="male"></a>
-</section>
-<section class="righttext">
-<h1>Yellow Daal</h1><p>The mung bean, alternatively known as the green gram, maash, or moong, is a plant species in the legume family.it is a slpited tentile which is very delicious serve with fresh corriander.</p>
-</section>
-</section>
-<section class="vegMenu">
-<section class="leftphoto">
-<a href="https://vegecravings.com/chole-recipe-chana-masala/"><img class="menuimage" src="images/chole-recipe-step-by-step-instructions-13.jpg" alt="male"></a>
-</section>
-<section class="righttext">
-<h1>White Chick Peas</h1><p>Chole also known as Chana Masala is a chickpea dish cooked in a spicy gravy. In this preparation kabuli chana and tomatoes are simmered with a mixture of fried garlic, ginger, onion and green chillies. It gets its authentic and unique taste from the addition of various Indian spices like cumin, bay leaf, cinnamon, chana masala, garam masala and turmeric.</p>
-</section>
-</section>
-<section class="vegMenu">
-<section class="leftphoto">
-<a href="https://www.safe.org.nz/recipe/vegetable-curry.html"><img class="menuimage" src="images/VEGETABLE-CURRY.jpg" alt="male"></a>
-</section>
-<section class="righttext">
-<h1>Mix Vegi</h1><p>Mix Veg Curry is made by cooking a mixture of vegetables together in a traditional Indian onion-tomato gravy.It can include seasonal vegetables and common vegetables like capsicum, cabbage, cauliflower, french beans, peas, carrots, potato, tomato, onion etc.The dish is characterized by multiple flavors due to many vegetables which also make it quite nutritious.</p>
-</section>
-</section>
-<section class="vegMenu">
-<section class="leftphoto">
-<a href="http://ashwita.com/recipes/recipes.php?q=Methi+Malai+Matar"><img class="menuimage" src="images/Methi-Malai-Matar.jpg" alt="Methi Malai Mattar"></a>
-</section>
-<section class="righttext">
-<h1>Methi Malai Mattar</h1><p>It is very flavorful and aromatic dish with creamy, smooth and delicious gravy. Aromatic because we have sauteed whole spices along with onions and ground them into paste. It is creamy because of the cashew nuts and heavy cream in it. And of course it is delicious.</p>
-</section>
-</section>
 
-<section class="vegMenu">
+$db=new database();
+$db::initialize("Menu");
+$menu=$db::getMeal();
+// $x=count($menu);   
+$days=array();
+$isNewDay=true;
+$i=0;
+echo' <div id="menu_div">';
+foreach($menu as $m)
+{
+    // echo $m->getItemDetail()."<br>";
+    // echo $m->getIngredient()."<br>";
+    
+    if(in_array($m->getDay(),$days))
+    {
+        $isNewDay=false;
+    }else{
+        array_push($days,$m->getDay());
+        $isNewDay=true;
+        $i=0;
+    }
+    $i++;
+    if($isNewDay)
+    {
+      echo '<div class="menutable"><div id="day_id">'.$m->getDay().'</div>';
+    }
+    if($m->getItemImage()!=null)
+    {
+      
+        $itemName = preg_replace('/\s/', '', $m->getItemName());
+        echo '<a href="#'.$itemName.'"><img src="data:image/jpg;base64,'.base64_encode($m->getItemImage()).'"/> </a>';
+    }
+    echo'<div class="hover_menu_img">
+    <p>'.$m->getItemName().' </p>
+    <p>$'. $m->getItemPrice().'</p>';   
+    echo' </div>';
+    if($i==3)
+    {    
+      echo' </div>';
+    }   
+    
+}
+echo' </div>';
+foreach($menu as $m)
+{
+  $itemName = preg_replace('/\s/', '', $m->getItemName());
+ echo '<section id="'.$itemName.'" class="vegMenu">
 <section class="leftphoto">
-<a href="http://www.prameelaskitchen.com/2015/03/whole-green-moong-dal-tadkacooked.html"><img class="menuimage" src="images//IMG_20150316_064733_2.jpg" alt="green mong daal"></a>
+<img style="height: 250px; width: 260px;" src="data:image/jpg;base64,'.base64_encode($m->getItemImage()).'"/>
 </section>
 <section class="righttext">
-<h1>Green Mong Daal</h1><p>Wholesome Green Moong Dal made in an Instant Pot or a Stovetop Pressure Cooker.  This is a delicious creamy lentil soup, that is so easy and quick to make in a pressure cooker. Very nutritious with lots of protein and perfect for a cold evening.</p>
+<h1>'.$m->getItemName().'</h1><p>'.$m->getItemDetail().'</p>
+<h3>Ingredients</h3><ul>';
+$ingredients=explode(",",$m->getIngredient());
+foreach($ingredients as $i)
+{
+echo '<li>'.$i.'</li>';
+}
+echo'</ul>
+<label for=""></label>
+-<input type="number" min="0" id="qty_item" placeholder="enter qty" />+
+<button type="submit" id="submit_'.$itemName.'">Add to Cart</button>
 </section>
-</section>
+</section>';
+}
+echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script> 
+$(document).ready(function(){
+ 
+  $("section.vegMenu").hide();
+$("a").click(function(event){
+  var itemName=$(this).attr("href");
+  $("section.vegMenu").hide();
+  itemName=itemName.replace(" ","");
+$("section"+itemName).show();
 
-</section>
-</div>
+})})
+</script>';
+
+?>
+
 	
 <!--Footer-->
 <?php
-include ("./views/footer.php")
+include ("./views/footer.php");
 ?>
