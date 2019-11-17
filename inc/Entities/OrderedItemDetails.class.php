c8a0ead9c50c57a1944ce73756687fa1853f9c36
charanjattana648 <singhc5@douglascollege.ca>
2019-11-17 10:04:55 -0800

    inc/Entities/OrderedItemDetails.class.php modified

---
+++ inc/Entities/OrderedItemDetails.class.php
@@ -0,0 +1,44 @@
+<?php
+class OrderedItemDetails{
+//$itemId, $orderId	$itemName	$qty	$price	$itemTotalPrice	$companyName
+private $itemId;
+private $orderId;
+private $itemName;
+private $qty;
+private $price;
+private $companyName;
+
+public function __construct(){}
+
+    /**setting all values */
+    public function addData($orderId, $itemName, $qty, $price, $companyName)
+    {
+        $this->orderId= $orderId;
+        $this->itemName= $itemName;
+        $this->qty= $qty;
+        $this->price= $price;
+        $this->itemTotalPrice= $itemTotalPrice;
+        $this->companyName= $companyName;
+    }
+
+
+    public function getitemId(){
+         return $this->itemId;
+        }
+    public function getorderId(){ 
+        return $this->orderId;
+    }
+    public function getitemName(){ 
+        return $this->itemName;
+    }
+    public function getqty(){
+         return $this->qty;
+        }
+    public function getprice(){
+         return $this->price;
+        }
+    public function getcompanyName(){ 
+        return $this->companyName;
+    }
+}
+?>
