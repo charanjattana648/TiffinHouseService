bd2ed77621c376eec88383edaa2c1e52edb911bd
charanjattana648 <singhc5@douglascollege.ca>
2019-11-19 00:06:42 -0800

    contact_us.js added for validation

---
+++ inc/controller/contact_us.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $("button#sendBtn_cu").click(function () {
+        var user_email = $("input#email_cu").val();
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!mailformat.test(user_email))
+        {
+            alert("Please enter a valid Email address!!");
+            return false;
+        }
+    });
+})
