57f10e9d1473522cd6f308f47fc7661973e27840
charanjattana648 <singhc5@douglascollege.ca>
2019-11-19 00:07:21 -0800

    header.js

---
+++ inc/controller/header.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    
+    console.log("header ready");
+    $("li.planMeal").mouseenter(function(){
+        $("ul.planUser").slideDown();
+    });
+
+    $("li.planMeal").mouseleave(function(){
+        $("ul.planUser").slideUp();
+    });
+
+    $("li.menuUser").mouseenter(function(){
+        $("ul.menuHover").slideDown();
+    });
+
+    $("li.menuUser").mouseleave(function(){
+        $("ul.menuHover").slideUp();
+    });
+
+})
